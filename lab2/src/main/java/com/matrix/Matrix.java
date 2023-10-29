233b7f16a0f4bee1889b587846646ccb1c474b29
Danylo Voychenko <danila.voychenko@gmail.com>
2023-10-30 00:34:01 +0200

    Lab2: Added Constructor, matrix coping

--- lab2/src/main/java/com/matrix/Matrix.java
+++ lab2/src/main/java/com/matrix/Matrix.java
@@ -1,4 +1,63 @@
 package com.matrix;
 
+
+
 public class Matrix {
+
+    private double[][] matrix;
+
+    public Matrix(){
+        this(0,0);        
+    }
+
+    public Matrix(int rows, int columns){
+        if(rows < 0 || columns < 0){
+            throw new IllegalArgumentException(String.format("Dimension can 0x0 but not: %dx%d",rows, columns));
+        }
+        if(rows == 0 ^ columns == 0){
+            throw new IllegalArgumentException(String.format("Dimension can't have only one 0: %dx%d",rows, columns));
+        }   
+        this.matrix = new double[rows][columns];
+    }
+
+    public Matrix(Matrix matrix_old){
+        this(matrix_old, new int[]{1,1}, new int[]{matrix_old.getNumRows(),matrix_old.getNumColumns()});
+    }
+
+    public Matrix(Matrix matrix_old, int[] start_elem, int[] end_elem){
+        if (start_elem[0] > matrix_old.getNumRows() || end_elem[0] > matrix_old.getNumColumns()){
+            throw new IllegalArgumentException(String.format("Element num is bigger than matrix dim"));
+        }
+        if (start_elem[0] > end_elem[0] || start_elem[1] > end_elem[1]){
+            throw new IllegalArgumentException(String.format("Num of start element must be smaller"));
+        }
+        int rows = end_elem[0] - start_elem[0] + 1;
+        int columns = end_elem[1] - start_elem[1] + 1;
+
+        this.matrix = new double[rows][columns];
+        int i_i = 0;
+        int j_j = 0;
+        for (int i = start_elem[0] - 1; i <= end_elem[0] - 1; i++, i_i++) {
+            j_j = 0;
+            for(int j = start_elem[1] - 1; j <= end_elem[1] - 1; j++, j_j++){
+                this.matrix[i_i][j_j] = matrix_old.matrix[i][j];
+            }
+        }
+
+    }
+
+    public int getNumRows(){
+        return matrix.length;
+    }
+
+    public int getNumColumns(){
+        if (getNumRows() == 0){
+            return 0;
+        }
+        return matrix[0].length;
+
+    }
+    public String getDimension(){
+        return String.valueOf(this.getNumRows()) + "x" + String.valueOf(this.getNumColumns()); 
+    }
 }
--- lab2/src/test/java/com/matrix/MatrixTest.java
+++ lab2/src/test/java/com/matrix/MatrixTest.java
@@ -1,5 +1,63 @@
 package com.matrix;
 
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
 public class MatrixTest {
+    private Matrix mat;
     
+    @Test
+    public void zeroMatrix(){
+        mat = new Matrix();
+        assertEquals("0x0", mat.getDimension());
+    }
+
+    @Test 
+    public void simpleMatrixConstruct(){
+        mat = new Matrix(3, 5);
+        assertEquals("3x5", mat.getDimension());
+    }
+
+    @Test
+    public void ThrowExceptionOnWrongDim(){
+    Exception exception = assertThrows(IllegalArgumentException.class,
+     () -> mat = new Matrix(-1, 2));
+        
+    String expectedMessage = "Dimension can 0x0 but not: -1x2";
+    String actualMessage = exception.getMessage();
+
+    assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    public void ThrowExceptionOnWrongDim_1(){
+    Exception exception = assertThrows(IllegalArgumentException.class,
+     () -> mat = new Matrix(1, 0));
+        
+    String expectedMessage = "Dimension can't have only one 0: 1x0";
+    String actualMessage = exception.getMessage();
+
+    assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test 
+    public void simpleCopyMatrixConstruct(){
+        mat = new Matrix(3, 5);
+        assertEquals("3x5", mat.getDimension());
+        Matrix mat_1 = new Matrix(mat);
+        assertEquals("3x5", mat_1.getDimension());
+    }
+    @Test
+    public void specificCopyMatrixConstuct(){
+        mat = new Matrix(3, 5);
+        assertEquals("3x5", mat.getDimension());
+        Matrix mat_1 = new Matrix(mat, new int[]{2, 1}, new int[]{2, 4});
+        assertEquals("1x4", mat_1.getDimension());
+    }
+    
+
 }
